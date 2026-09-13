3d9c1e937f1d40ef0c36f52bd2a6d9c2593ff20d
joao pedro <joao.pedrozg@hotmail.com>
2026-09-13 20:45:11 -0300

    Created(NewCampaign)

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -10,5 +10,21 @@ type Campaign struct {
 	ID        string
 	Name      string
 	CreatedOn time.Time
+	Content   string
 	Contacts  []Contact
+}
+
+func NewCampaign(name string, content string, emails []Contact) *Campaign {
+
+	contacts := make([]Contact, len(emails))
+	for index, email := range emails {
+		contacts[index].Email = email.Email
+	}
+	return &Campaign{
+		ID:        "",
+		Name:      name,
+		CreatedOn: time.Now(),
+		Content:   content,
+		Contacts:  contacts,
+	}
 }
